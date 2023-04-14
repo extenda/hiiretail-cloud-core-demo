5ae3c2f99a2db695fbace1e66c3c3a65117e09e1
glebbash <glebbash@gmail.com>
2023-04-14 18:40:30 +0000

    fix(ccc): update base path

--- ccc/build/_app/immutable/chunks/1.6185f888.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.3844b70b.js";export{t as component};
--- ccc/svelte.config.js
+++ ccc/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,7 +18,10 @@ const config = {
       fallback: "index.html",
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+      base: dev ? '' : '/extenda/hiiretail-cloud-core-demo/master/ccc/build',
+    }
 	}
 };
 
