2477faf2dc4fe07222d95a52289c0d89ee900fb5
glebbash <glebbash@gmail.com>
2023-04-14 18:53:49 +0000

    fix(ccc): drop double prefix

--- ccc/svelte.config.js
+++ ccc/svelte.config.js
@@ -20,7 +20,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: dev ? '' : '/extenda/hiiretail-cloud-core-demo/master/ccc/build',
+      base: dev ? '' : '/extenda/hiiretail-cloud-core-demo/master/ccc',
     }
 	}
 };
