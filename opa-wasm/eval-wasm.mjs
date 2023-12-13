4d6cd0266c56934d38b6cdb2b711e6a42b528a17
Gleb Bashkatov <glebbash@gmail.com>
2023-12-13 15:21:42 +0000

    feat: make working version of eval-wasm

--- opa-wasm/eval-wasm.mjs
+++ opa-wasm/eval-wasm.mjs
@@ -5,7 +5,7 @@ import jsonwebtoken from "jsonwebtoken";
 const policy = await opa.loadPolicy(
   await fs.readFile("out/policy.wasm"),
   undefined,
-  buildCustomBuiltins()
+  makeCustomBuiltins()
 );
 policy.setData(JSON.parse(await fs.readFile("out/data.json")));
 
@@ -20,19 +20,17 @@ console.dir(res, { depth: 10 });
 // }
 // console.timeEnd("evaluate 10K");
 
-function buildCustomBuiltins() {
+function makeCustomBuiltins() {
   return {
+    "time.now_ns": () => Date.now() * 1000,
     "io.jwt.decode": (jwt) => {
       const decoded = jsonwebtoken.decode(jwt, { complete: true });
       if (decoded === null) {
         return [{}, {}, {}];
       }
-
       return [decoded.header, decoded.payload, decoded.signature];
     },
     "io.jwt.decode_verify": (jwt, { cert, iss, aud, time, ...other }) => {
-      // TODO: not sure what to do with time
-      const _time = time;
       if (Object.keys(other).length !== 0) {
         throw new Error(
           "Unknown constraints: " + Object.keys(other).join(", ")
@@ -48,33 +46,19 @@ function buildCustomBuiltins() {
           }
 
           const { keys } = JSON.parse(cert);
-          key = keys[decoded.header.kid]?.n;
+          key = keys.find((k) => k.kid === decoded.header.kid)?.n;
         }
 
         const { header, payload } = jsonwebtoken.verify(jwt, key, {
           issuer: iss,
           audience: aud,
-          // TODO: not sure what to do with time
-          // clockTimestamp: time !== undefined ? time / 1000 : undefined,
+          complete: true,
+          clockTimestamp: time !== undefined ? time / 1_000_000 : undefined,
         });
-
-        // console.log("OK(io.jwt.decode_verify)", { cert, iss, aud, time }, "=", {
-        //   ok: true,
-        // });
-        // console.log("ok");
         return [true, header, payload];
-      } catch (error) {
-        // console.log(
-        //   "ERROR(io.jwt.decode_verify)",
-        //   { cert: cert.slice(0, 10) + "...", iss, aud, time },
-        //   "=",
-        //   { error: error.message }
-        // );
-        // const decoded = jsonwebtoken.decode(jwt, { complete: true });
-        // console.log("err", { error: error.message, decoded, cert });
+      } catch (_error) {
         return [false, {}, {}];
       }
     },
-    "time.now_ns": () => Date.now() * 1000,
   };
 }
