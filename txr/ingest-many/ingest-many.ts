57a7aa88305095e76524aeb23068ab483e94e9fc
Gleb Bashkatov <glebbash@gmail.com>
2025-01-20 10:03:56 +0000

    chore: update a script for deno 2

--- txr/ingest-many/ingest-many.ts
+++ txr/ingest-many/ingest-many.ts
@@ -1,67 +1,68 @@
-import { DOMParser } from "https://esm.sh/linkedom";
-import { resolve } from "https://deno.land/std@0.136.0/path/mod.ts";
-import { format } from "https://deno.land/std@0.136.0/datetime/mod.ts";
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
-import { config } from "./config.ts";
-import { zip } from "../utils.ts";
-import { publishTransaction } from "../api.ts";
-import { TENANT_ID } from "../constants.ts";
+import { sleep } from 'jsr:@so-dang-cool/sleep@1.3.0';
+import { format } from 'jsr:@std/datetime@0.225.0';
+import { resolve } from 'jsr:@std/path@0.225.0';
+import { DOMParser } from 'npm:linkedom@0.18.6';
 
-const transactionDir = resolve("./ingest-many/transactions");
+import { publishTransaction } from '../api.ts';
+import { TENANT_ID } from '../constants.ts';
+import { zip } from '../utils.ts';
+import { config } from './config.ts';
+
+const transactionDir = resolve('./ingest-many/transactions');
 const transactions = Deno.readDir(transactionDir);
-let previousTransactionId = "FIRST_TRANSACTION";
+let previousTransactionId = 'FIRST_TRANSACTION';
 
 for await (const transaction of transactions) {
   const file = await Deno.readTextFile(`${transactionDir}/${transaction.name}`);
-  const document = new DOMParser().parseFromString(file, "text/xml");
+  const document = new DOMParser().parseFromString(file, 'text/xml');
   const now = new Date();
-  const nowStr = format(now, "yyyy-MM-ddTHH:mm:ss");
+  const nowStr = format(now, 'yyyy-MM-ddTHH:mm:ss');
 
-  const bu =
-    document.getElementsByTagName("BusinessUnit")[0].getElementsByTagName(
-      "UnitID",
-    )[0];
-  bu.setAttribute("Name", config.businessUnitId || bu.getAttribute("Name"));
-  const businessUnit = bu.getAttribute("Name");
+  const bu = document
+    .getElementsByTagName('BusinessUnit')[0]
+    .getElementsByTagName('UnitID')[0];
+  bu.setAttribute('Name', config.businessUnitId || bu.getAttribute('Name'));
+  const businessUnit = bu.getAttribute('Name');
 
-  const ws = document.getElementsByTagName("WorkstationID")[0];
+  const ws = document.getElementsByTagName('WorkstationID')[0];
   ws.innerHTML = config.workstationId || ws.innerHTML;
   const workstation = ws.innerHTML;
 
-  const o = document.getElementsByTagName("OperatorID")[0];
+  const o = document.getElementsByTagName('OperatorID')[0];
   o.innerHTML = config.operatorId || o.innerHTML;
 
-  const sn = document.getElementsByTagName("SequenceNumber")[0];
+  const sn = document.getElementsByTagName('SequenceNumber')[0];
   sn.innerHTML = (config.sequenceNumber++).toString();
   const sequenceNumber = sn.innerHTML;
 
-  const rn = document.getElementsByTagName("ReceiptNumber")[0];
+  const rn = document.getElementsByTagName('ReceiptNumber')[0];
   rn.innerHTML = (config.receiptNumber++).toString();
   const receiptNumber = rn.innerHTML;
 
-  const tid = document.getElementsByTagName("TransactionID")[0];
-  tid.innerHTML = `${businessUnit};${workstation};${sequenceNumber};${
-    nowStr.replace("T", "")
-  };${receiptNumber}`;
+  const tid = document.getElementsByTagName('TransactionID')[0];
+  tid.innerHTML = `${businessUnit};${workstation};${sequenceNumber};${nowStr.replace(
+    'T',
+    '',
+  )};${receiptNumber}`;
   const transactionId = tid.innerHTML;
 
-  const beginDate = document.getElementsByTagName("BeginDateTime")[0];
+  const beginDate = document.getElementsByTagName('BeginDateTime')[0];
   beginDate.innerHTML = nowStr;
 
-  const endDate = document.getElementsByTagName("EndDateTime")[0];
+  const endDate = document.getElementsByTagName('EndDateTime')[0];
   endDate.innerHTML = nowStr;
 
   const data = zip(new TextEncoder().encode(document.toString()))!;
 
   await publishTransaction(data, {
-    "transaction-timestamp": now.toISOString(),
-    "country-code": config.countryCode || "NO",
-    "content-type": "application/zip",
-    "correlation-id": crypto.randomUUID(),
-    "tenant-id": config.tenantId || TENANT_ID,
-    "business-unit-id": businessUnit,
-    "transaction-id": transactionId,
-    "previous-transaction-id": previousTransactionId,
+    'transaction-timestamp': now.toISOString(),
+    'country-code': config.countryCode || 'NO',
+    'content-type': 'application/zip',
+    'correlation-id': crypto.randomUUID(),
+    'tenant-id': config.tenantId || TENANT_ID,
+    'business-unit-id': businessUnit,
+    'transaction-id': transactionId,
+    'previous-transaction-id': previousTransactionId,
   });
 
   console.log(
@@ -70,5 +71,5 @@ for await (const transaction of transactions) {
 
   previousTransactionId = transactionId;
 
-  await sleep(config.msBetweenIngests / 1000);
+  await sleep.millis(config.msBetweenIngests / 1000);
 }
